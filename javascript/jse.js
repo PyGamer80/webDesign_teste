5d27cfdd978ffcac0295cf5c9bdf2da79f8cd4d4
pygamer80 <pygamer80@gmail.com>
2022-06-03 19:01:56 -0300

    html pronto

---
+++ javascript/jse.js
@@ -0,0 +1,16 @@
+
+function minhaFunção(){
+   alert("Você clicou!");
+}
+
+function testeclasse(){
+   var x = document.getElementsByClassName("city");
+   for (var i =0; i < x.length; i++){
+       x[i].style.display = "none"
+   }
+
+}
+
+function myFunction() { 
+  document.getElementById("demo").innerHTML = "Hello JavaScript!";
+}
